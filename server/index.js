94efd59f9da8568b985a2a9baae5012293a8b024
owkmear <h3s8qp9xw3f@gmail.com>
2021-08-17 14:25:13 +0300

    Refactor server

--- server/index.js
+++ server/index.js
@@ -1,227 +1,205 @@
-const express = require('express');
-const async = require('async');
-const app = express();
-var Cookies = require('cookies');
+const express = require('express')
+const app = express()
+const Cookies = require('cookies')
 
 // TODO: add NODE_ENV file
-const ARTIFICIAL_DELAY = 1000;
-const PORT = '4500';
-const PROJECT_NAME = 'Coininfo';
-const EXPIRES_COOKIE = 14 * 24 * 60 * 60 * 1000; // 14 days
+const ARTIFICIAL_DELAY = 1000
+const PORT = '4500'
+const PROJECT_NAME = 'Coininfo'
+const EXPIRES_COOKIE = 14 * 24 * 60 * 60 * 1000 // 14 days
 
-app.use(express.json());
-app.use(Cookies.express());
-app.use(function(req, res, next) {
-  setTimeout(next, ARTIFICIAL_DELAY);
-});
+app.use(express.json())
+app.use(Cookies.express())
+app.use(function (req, res, next) {
+  setTimeout(next, ARTIFICIAL_DELAY)
+})
 
 // Uncomment to disable security
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    next();
-});
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  next()
+})
 
 class DBContext {
-  database = [];
+  database = []
   constructor() {
     this.database.push({
       username: 'Michael',
       email: 'Michael@gmail.com',
-      password: '1234',
-    });
+      password: '1234'
+    })
     this.database.push({
       username: 'John',
       email: 'John@gmail.com',
-      password: 'qwerty',
-    });
+      password: 'qwerty'
+    })
   }
 
   haveUser(email, password) {
     for (const user of this.database)
-      if (user.email === email && user.password === password) return user;
-    return false;
+      if (user.email === email && user.password === password) return user
+    return false
   }
 
   checkUser(email) {
-    for (const user of this.database) if (user.email === email) return true;
-    return false;
+    for (const user of this.database) if (user.email === email) return true
+    return false
   }
 
   createUser(username, email, password) {
-    this.database.push({ username, email, password });
+    this.database.push({ username, email, password })
   }
 }
 
-let DB = new DBContext();
+let DB = new DBContext()
 
-app.get('/ping', function(req, res) {
-  console.log('Post /ping');
-  res.type('json');
-  res.header('Content-Type', 'text/html; charset=utf-8');
-  res.send('pong');
-});
+app.get('/ping', function (req, res) {
+  console.log('Post /ping')
+  res.type('json')
+  res.header('Content-Type', 'text/html; charset=utf-8')
+  res.send('pong')
+})
 
-app.post('/api/v1/login', function(req, res) {
-  console.log('Post /api/v1/login');
-  res.type('json');
-  res.header('Content-Type', 'text/html; charset=utf-8');
-  const { email, password } = req.body;
-  let answer = null;
-  const user = DB.haveUser(email, password);
+app.post('/api/v1/login', function (req, res) {
+  console.log('Post /api/v1/login')
+  res.type('json')
+  res.header('Content-Type', 'text/html; charset=utf-8')
+  const { email, password } = req.body
+  let answer = null
+  const user = DB.haveUser(email, password)
   if (user) {
     answer = {
       success: true,
       data: {
         email: user.email,
-        username: user.username,
-      },
-    };
-    let expiresDate = new Date();
-    expiresDate.setTime(expiresDate.getTime() + EXPIRES_COOKIE);
+        username: user.username
+      }
+    }
+    let expiresDate = new Date()
+    expiresDate.setTime(expiresDate.getTime() + EXPIRES_COOKIE)
     req.cookies.set(`${PROJECT_NAME}_email`, user.email, {
-      expires: expiresDate,
-    });
+      expires: expiresDate
+    })
     req.cookies.set(`${PROJECT_NAME}_username`, user.username, {
-      expires: expiresDate,
-    });
+      expires: expiresDate
+    })
     req.cookies.set(`${PROJECT_NAME}_isLogined`, 'true', {
-      expires: expiresDate,
-    });
+      expires: expiresDate
+    })
   } else {
     answer = {
-      success: false,
-    };
+      success: false
+    }
   }
-  res.send(answer);
-});
+  res.send(answer)
+})
 
-app.post('/api/v1/isLogined', function(req, res) {
-  console.log('Post /api/v1/isLogined');
-  res.type('json');
-  res.header('Content-Type', 'text/html; charset=utf-8');
+app.post('/api/v1/isLogined', function (req, res) {
+  console.log('Post /api/v1/isLogined')
+  res.type('json')
+  res.header('Content-Type', 'text/html; charset=utf-8')
 
-  const email = req.cookies.get(`${PROJECT_NAME}_email`);
-  const username = req.cookies.get(`${PROJECT_NAME}_username`);
-  const isLogined = req.cookies.get(`${PROJECT_NAME}_isLogined`);
+  const email = req.cookies.get(`${PROJECT_NAME}_email`)
+  const username = req.cookies.get(`${PROJECT_NAME}_username`)
+  const isLogined = req.cookies.get(`${PROJECT_NAME}_isLogined`)
 
-  let answer = null;
+  let answer = null
   if (isLogined) {
     answer = {
       success: true,
       data: {
         email: email,
-        username: username,
-      },
-    };
+        username: username
+      }
+    }
   } else {
     answer = {
-      success: false,
-    };
+      success: false
+    }
   }
 
-  res.send(answer);
-});
+  res.send(answer)
+})
 
-app.post('/api/v1/logout', function(req, res) {
-  console.log('Post /api/v1/logout');
-  res.type('json');
-  res.header('Content-Type', 'text/html; charset=utf-8');
+app.post('/api/v1/logout', function (req, res) {
+  console.log('Post /api/v1/logout')
+  res.type('json')
+  res.header('Content-Type', 'text/html; charset=utf-8')
   let answer = {
-    success: true,
-  };
+    success: true
+  }
 
-  let expiresDate = new Date();
-  expiresDate.setTime(0);
+  let expiresDate = new Date()
+  expiresDate.setTime(0)
   req.cookies.set(`${PROJECT_NAME}_email`, 'Michael@gmail.com', {
-    expires: expiresDate,
-  });
+    expires: expiresDate
+  })
   req.cookies.set(`${PROJECT_NAME}_username`, 'Michael', {
-    expires: expiresDate,
-  });
+    expires: expiresDate
+  })
   req.cookies.set(`${PROJECT_NAME}_isLogined`, 'true', {
-    expires: expiresDate,
-  });
+    expires: expiresDate
+  })
 
-  res.send(answer);
-});
+  res.send(answer)
+})
 
-app.post('/api/v1/registration', function(req, res) {
-  console.log('Post /api/v1/registration');
-  res.type('json');
-  res.header('Content-Type', 'text/html; charset=utf-8');
-  const { username, email, password } = req.body;
-  let answer = null;
-  const haveUser = DB.checkUser(email);
+app.post('/api/v1/registration', function (req, res) {
+  console.log('Post /api/v1/registration')
+  res.type('json')
+  res.header('Content-Type', 'text/html; charset=utf-8')
+  const { username, email, password } = req.body
+  let answer = null
+  const haveUser = DB.checkUser(email)
   if (!haveUser) {
-    DB.createUser(username, email, password);
+    DB.createUser(username, email, password)
     answer = {
       success: true,
       data: {
         email: email,
-        username: username,
-      },
-    };
-    let expiresDate = new Date();
-    expiresDate.setTime(expiresDate.getTime() + EXPIRES_COOKIE);
+        username: username
+      }
+    }
+    let expiresDate = new Date()
+    expiresDate.setTime(expiresDate.getTime() + EXPIRES_COOKIE)
     req.cookies.set(`${PROJECT_NAME}_email`, email, {
-      expires: expiresDate,
-    });
+      expires: expiresDate
+    })
     req.cookies.set(`${PROJECT_NAME}_username`, username, {
-      expires: expiresDate,
-    });
+      expires: expiresDate
+    })
     req.cookies.set(`${PROJECT_NAME}_isLogined`, 'true', {
-      expires: expiresDate,
-    });
+      expires: expiresDate
+    })
   } else {
     answer = {
-      success: false,
-    };
+      success: false
+    }
   }
-  res.send(answer);
-});
+  res.send(answer)
+})
 
 app.listen(PORT, () => {
-  console.log(`Server start on port ${PORT}`);
-});
+  console.log(`Server start on port ${PORT}`)
+})
 
-var stdin = process.openStdin();
-stdin.addListener('data', function(data) {
-  let input = data
-    .toString()
-    .trim()
-    .toLowerCase();
+const stdin = process.openStdin()
+stdin.addListener('data', function (data) {
+  let input = data.toString().trim().toLowerCase()
 
   if (input.slice(0, 1) === 's' && input.length > 2) {
-    console.log('command:');
-    console.log(app[input.split(' ')[1]]);
+    console.log('command:')
+    console.log(app[input.split(' ')[1]])
   } else {
     switch (input) {
       case 'db':
-        console.log('Database:');
-        console.log(DB);
-        // console.log('cachedMobgetcurrencies:');
-        // console.log(cachedMobgetcurrencies);
-        // console.log('\ncachedGetArticles:');
-        // console.log(cachedGetArticles);
-        // console.log('\ncachedData:');
-        // console.log(cachedData);
-        break;
+        break
       case 'time':
-        console.log('Time');
-
-        // console.log('time:');
-        // console.log(time);
-        // console.log('\ngetArticlesCacheTime:');
-        // console.log(getArticlesCacheTime);
-        // console.log('\ngetArticlesConcreteCacheTime:');
-        // console.log(getArticlesConcreteCacheTime);
-        // console.log('\ngetDataCacheTime:');
-        // console.log(getDataCacheTime);
-        break;
+        break
       default:
-        console.log('Unknown comand: ' + input);
+        console.log('Unknown command: ' + input)
     }
   }
-});
+})
